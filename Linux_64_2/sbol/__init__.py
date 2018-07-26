55683aaf9ca3e65a2aa9c8b3e48b82da8e8773ce
Bryan Bartley <bartleyba@sbolstandard.org>
2018-07-26 05:14:28 +0000

    Build manylinux wheels post19

--- Linux_64_2/sbol/__init__.py
+++ Linux_64_2/sbol/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 __version__ = '2.3.0'
+__commit__ = '3d4fc608aa4c2cfe23a02cf37acd83ecaeac0a34'
 
-from sbol.libsbol import *
+from sbol.sbol import *
 import sbol.unit_tests
