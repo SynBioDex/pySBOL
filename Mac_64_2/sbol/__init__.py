0c3bf8cb16a293c8b6443dffefc4f8ceb62c65ce
Bryan Bartley <bryan.a.bartley@raytheon.com>
2018-07-29 02:18:42 -0400

    Rebuild MacOSX post19

--- Mac_64_2/sbol/__init__.py
+++ Mac_64_2/sbol/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 __version__ = '2.3.0'
-__commit__ = '3d4fc608aa4c2cfe23a02cf37acd83ecaeac0a34'
+__commit__ = '15128fedd6f6f7e8189e7ffdf61910b276e7e765'
 
 from sbol.sbol import *
 import sbol.unit_tests
