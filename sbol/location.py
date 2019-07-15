0d01d6f6ae4772eaea14788fa77089a5032e19cc
llotneb <tollben@umich.edu>
2019-07-15 13:59:49 -0400

    Passing 83 roundtrip tests.
    Simply made Location extend Identified.

--- sbol/location.py
+++ sbol/location.py
@@ -1,5 +1,14 @@
-class Location:
-    pass
+from identified import Identified
+from constants import *
+from property import Property
+from rdflib import URIRef
+
+class Location(Identified):
+
+    def __init__(self, type_uri=SBOL_LOCATION, uri=URIRef('example'), orientation=SBOL_ORIENTATION_INLINE):
+        super().__init__(type_uri, uri)
+        self.orientation = Property(self, SBOL_ORIENTATION, '1', '1', [], orientation)
+        self.sequence = Property(self, SBOL_SEQUENCE_PROPERTY, SBOL_SEQUENCE, '0', '1', [])
 
 
 class Range(Location):
